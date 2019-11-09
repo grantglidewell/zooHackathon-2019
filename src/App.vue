4916959943f8eb2c628b8288235d0a4cca012b86
Grant Glidewell <ggcadc@gmail.com>
2019-11-09 13:24:24 -0800

    added form, working to add data to table

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div id="app"></div>
+  <div>
+    <div id="app">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
 <script>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,18 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import Form from '@/components/Form';
 
 Vue.use(Router);
 
 export default new Router({
-  linkExactActiveClass: "active",
-  mode: "history",
-  routes: []
+  linkExactActiveClass: 'active',
+  mode: 'history',
+  routes: [
+    {
+      path: '/form',
+      name: 'Form',
+      component: Form
+    }
+  ]
 });
