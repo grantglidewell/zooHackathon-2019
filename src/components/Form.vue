5cf03fa67ea4a7f8ce970cd944ea5e0370f3eb46
Grant Glidewell <ggcadc@gmail.com>
2019-11-09 16:19:54 -0800

    added reference to endangered species list to elevate reports

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -7,13 +7,16 @@
         <base-input v-model="price" label="Animal Price" style="flex: 1" />
       </span>
       <base-button @click="onSubmit">add</base-button>
+      <span v-if="endangered" class="text-red">This animal is possibly endangered</span>
     </card>
   </div>
 </template>
 <script>
+/* eslint-disable no-console */
 import { addTableRow } from "../util/api";
+import { debounce } from "../util/debounce";
 export default {
-  data: () => ({ url: "", name: "", price: "" }),
+  data: () => ({ url: "", name: "", price: "", endangered: false }),
   methods: {
     onSubmit: async function() {
       if (!this.url || !this.name || !this.price) {
@@ -25,7 +28,8 @@ export default {
             user: this.$store.state.email,
             url: this.url,
             animalName: this.name,
-            animalPrice: this.price
+            animalPrice: this.price,
+            endangered: this.endangered
           }
         }
       ]);
@@ -35,6 +39,26 @@ export default {
       this.url = "";
       this.name = "";
       this.price = "";
+    },
+    handleIsEndangered: debounce(({ name, list, callback }) => {
+      const names = name.split(" ");
+      const isEndangered = names.filter(n => {
+        if (list.includes(n)) {
+          return true;
+        }
+      });
+      return callback(Boolean(isEndangered.length));
+    }, 250)
+  },
+  watch: {
+    name(value) {
+      if (value.length > 3) {
+        this.handleIsEndangered({
+          name: value,
+          list: this.$store.state.endangeredSpecies,
+          callback: val => (this.endangered = val)
+        });
+      }
     }
   }
 };
